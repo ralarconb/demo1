7ec2d0ec08728679c39aff2b8dfadabe95529537
ralarconb <ralarconbazan@gmail.com>
2022-05-16 21:48:42 -0500

    .

---
+++ src/main/java/com/abh/jpa/demo1/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.abh.jpa.demo1;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+	List<Customer> findByLastName(String lastName);
+
+	Customer findById(long id);
+}
